7b4aed90f4465c9a33214c5b30cc5b4344d7051e
Hitman666 <nikola.breznjak@gmail.com>
2014-12-30 19:37:47 +0100

    factories and resources!

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express'),
+	api = require('./api'),
 	app = express();
 
 app
 	.use(express.static('./public'))
+	.use('/api', api)
 	.get('*', function(req, res) {
 		res.sendFile(__dirname + '/public/main.html');
 	})
